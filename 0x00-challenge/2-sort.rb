45c802b8d772cb64272aa2c171645ea4173c5edb
laamyry <slamiri10@gmail.com>
2023-08-24 00:21:57 +0100

    laamyry py

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -18,11 +18,11 @@ ARGV.each do |arg|
     l = result.size
     while !is_inserted && i < l do
         if result[i] < i_arg
-            i += 1
-        else
             result.insert(i - 1, i_arg)
             is_inserted = true
             break
+        else
+            i += 1
         end
     end
     result << i_arg if !is_inserted
